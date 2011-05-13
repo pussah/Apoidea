84e4a7f6e477114961d21feb0c3408548ea193e1
Magnus Holmstrom <lodis_mange@hotmail.com>
2011-05-13 11:18:33 +0200

    Mohaha

--- src/apoidea.erl
+++ src/apoidea.erl
@@ -3,9 +3,9 @@
 
 start() -> 
     io:format("=========== Welcome to Apoidea ===========\nFor a list of available commands, type 'help'"),
-    start1().
+    start1("", "").
 
-start1() ->
+start1(Key, Sock) ->
     UserCommand = io:get_line("\nApoidea> "),
     case UserCommand of
 	"/help\n" -> io:format("Commands:\n/init - Initiates a connection.\n/download - Download a file.\n/back - Return to previous menu.\n/quit - Quit Apoidea.\n");
@@ -13,21 +13,21 @@ start1() ->
 		     IPadress = io:get_line("\nApoidea> "),
 		     IP_adress = string:substr(IPadress, 1, length(IPadress) - 1),
 		     case IP_adress of
-			 "/back" -> start1();
+			 "/back" -> start1(Key, Sock);
 			 "/quit" -> init:stop();
-			 Other -> {Key, Sock} = worker:init(IP_adress, 5678)
+			 Other -> {Key_1, Sock_1} = worker:init(IP_adress, 5678), start1(Key_1, Sock_1)
 		     end,
-		     start1();
+		     start1(Key, Sock);
 	"/download\n" -> io:format("\nDownload\n\nEnter the name of the file you wish to download: "),
 			 FileName = io:get_line("\nApoidea> "),
 			 case FileName of
-			     "/back\n" -> start1();
+			     "/back\n" -> start1(Key, Sock);
 			     "/quit\n" -> init:stop();
-			     Other -> worker:start_downloader(FileName, "", "")
+			     Other -> worker:start_downloader(FileName, Key, Sock)
 			 end,
-			 start1();
+			 start1(Key, Sock);
 	"/quit\n" -> init:stop();
-	"/back\n" -> start1();
+	"/back\n" -> start1(Key, Sock);
 	Other -> io:format("Invalid choise.\n")
     end,
-    start1().
+    start1(Key, Sock).
