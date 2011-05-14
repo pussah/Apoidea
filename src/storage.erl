1313d3f614b20fba36aa7ab21d9e3724aa2899d7
LoppacC <robin.broacc@gmail.com>
2011-05-14 18:48:28 +0200

    Update of storage.erl

--- src/storage.erl
+++ src/storage.erl
@@ -1,21 +1,34 @@
 -module(storage).
--export([start/0, table/0, add_file/2, find_file/2, add_crap/0]).
+-export([table/0, add_file/2,  all/1,  add_crap/0]).
 -include_lib("eunit/include/eunit.hrl").
 
 
 
 table() ->
-	ets:new('FileList', [bag]).
+	ets:new('FileList', []).
 
 add_file(FileMap, {IP, Port, Name, Peices, Possessions}) ->
-	ets:insert(FileMap, {Name, IP, Port, Peices, Possessions}).
+	ets:insert(FileMap, {{Name, IP}, Port, Peices, Possessions}).
 
-find_file(FileMap, FileName) ->
-	ets:lookup(FileMap, FileName).
+%find_peice(FileMap, FileName, Peice) ->
+%	ok.
+%   ets:match(FileMap, {{FilenName, '$2'}, '$3', '$4', '$5'}).
 
 all(FileMap) ->
-	ets:match(FileMap, {'$1', '$2', '$3', '$4', '$5'}).
+	ets:tab2list(FileMap).
 
+find_file(FileMap, File) ->
+	ets:match(FileMap, {{File, '$2'}, '$3', '$4', '$5'}).
+
+
+client_del_file(FileMap, Key) ->
+	ets:delete(FileMap,Key).
+
+del_client(FileMap, IP) ->
+	ets:match_delete(FileMap, {{'_', IP}, '_', '_', '_'}).
+	%ets:delete_object(FileMap, {{'_', IP}, '_', '_', '_'}).
+	%ets:delete(FileMap, {'_', IP}).
+	
 add_crap() ->
 	FileMap = table(),
 	add_file(FileMap, {"localhost", 80, "Testfile", 10, all}),
@@ -24,7 +37,25 @@ add_crap() ->
 	add_file(FileMap, {"192.168.0.3", 1236, "NotAVirus.bat", 5, [1,2,3,4]}),
 	add_file(FileMap, {"192.168.0.1", 1234, "Wc3.exe", 50, all}),
 	add_file(FileMap, {"192.168.0.4", 5432, "Testfile", 10, all}),
-	X = find_file(FileMap, "Testfile"),
+	add_file(FileMap, {"192.168.0.4", 5432, "Skype.exe", 5, all}),
+	O = "Testfile",
+	X = find_file(FileMap, O),
 	io:format("~n-Testfile- found at: ~p ~n", [X]),
+	Z = all(FileMap),
+	io:format("~n-The entire FileMap: ~p ~n", [Z]),
+	P = "WoW.exe",
+	io:format("~n-Client deleted: ~s ~n", [P]),
+	client_del_file(FileMap, {P, "192.168.0.1"}),
 	Y = all(FileMap),
-	io:format("~n-The entire FileMap: ~p ~n", [Y]).
+	io:format("~n-The entire FileMap: ~p ~n", [Y]),
+	IP = "192.168.0.4",
+	io:format("~n-Client <~s> disconnected ~n", [IP]),
+	del_client(FileMap, IP),
+	K = all(FileMap),
+	io:format("~n-The entire FileMap: ~p ~n", [K]).	  
+%	Z = find_IP(FileMap, "192.168.0.1"),
+%	io:format("~n-IP x.x.x.1 : ~p ~n", [Z]),
+%	K = del_client(FileMap, "192.168.0.1"),
+%	io:format("Deleted rows: ~w~n", [K]),
+%	Q = all(FileMap),
+%	io:format("~n-The entire FileMap: ~p ~n", [Q]).
