f37c8f83a0be9e0be5ae42132a8293556f9f27e4
LoppacC <robin.broacc@gmail.com>
2011-05-14 16:39:52 +0200

    Small changes

--- src/storage.erl
+++ src/storage.erl
@@ -16,39 +16,6 @@ find_file(FileMap, FileName) ->
 all(FileMap) ->
 	ets:match(FileMap, {'$1', '$2', '$3', '$4', '$5'}).
 
-
-
-
-
-
-start() -> start( mouse ).
-
-start( Animal ) ->
-        Kingdom = ets:new( 'magic',  [] ),
-        % note: table is not square
-        populate( Kingdom, [{micky,mouse}, {mini,mouse}, {goofy}] ),
-        Member = ets:member( Kingdom, micky ),
-        io:format( " member ~w ~n ", [ Member ] ),
-        show_next_key( Kingdom, micky ),
-        find_animal( Kingdom, Animal ).
-        
-show_next_key( _Kingdom, '$end_of_table' ) -> done;
-show_next_key( Kingdom,  Key) ->
-        Next = ets:next( Kingdom, Key ),
-        io:format( " next ~w ~n ", [ Next ] ),
-        show_next_key( Kingdom, Next ).
-
-populate( _Kingdom, [] ) -> {done,start};
-populate( Kingdom, [H | T] ) ->
-                ets:insert( Kingdom, H ),
-                populate( Kingdom, T ).
-        
-find_animal( Kingdom, Animal ) ->
-        ets:match( Kingdom, { '$1', Animal } ).
-
-
-
-
 add_crap() ->
 	FileMap = table(),
 	add_file(FileMap, {"localhost", 80, "Testfile", 10, all}),
